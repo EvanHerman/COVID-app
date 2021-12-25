c0e41078d68ba460b57f0e14caf678c9eb069fa4
Evan Herman <evan.m.herman@gmail.com>
2021-12-25 13:22:31 -0500

    Store API request data in localstorage

--- src/index.js
+++ src/index.js
@@ -6,26 +6,40 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 
-// Get all COVID-19 data
-fetch( "https://www.covid19-api.com/country/all?format=json", {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
-  }
-} )
-.then( ( covidData ) => covidData.json() )
-.then( covidData => {
-  // Get geolocation data for user
-  fetch( "https://ipapi.co/json/", {
-    "method": "GET"
-  } )
-  .then( ( geoLocationData ) => geoLocationData.json() )
-  .then( geoLocationData => {
+// Check for API cache
+if ( localStorage.getItem( "covid-19-cache" ) !== null ) {
+  let covidCache = JSON.parse( localStorage.getItem( "covid-19-cache" ) );
+  if ( !! covidCache.covidData && !! covidCache.geoLocationData ) {
     ReactDOM.render(
       <BrowserRouter>
-        <App covidData={covidData} geoLocationData={geoLocationData} />
+        <App covidData={covidCache.covidData} geoLocationData={covidCache.geoLocationData} />
       </BrowserRouter>,
-     document.getElementById( 'root' )
+     document.getElementById( "root" )
     );
+  }
+} else {
+  // Retreive all COVID-19 data
+  fetch( "https://www.covid19-api.com/country/all?format=json", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+    }
+  } )
+  .then( ( covidData ) => covidData.json() )
+  .then( covidData => {
+    // Get geolocation data for user
+    fetch( "https://ipapi.co/json/", {
+      "method": "GET"
+    } )
+    .then( ( geoLocationData ) => geoLocationData.json() )
+    .then( geoLocationData => {
+      window.localStorage.setItem( "covid-19-cache", JSON.stringify( { covidData: covidData, geoLocationData: geoLocationData } ) );
+      ReactDOM.render(
+        <BrowserRouter>
+          <App covidData={covidData} geoLocationData={geoLocationData} />
+        </BrowserRouter>,
+       document.getElementById( "root" )
+      );
+    } );
   } );
-} );
+}
